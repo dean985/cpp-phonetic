893286a61a254ff90296284af3044b26dc9e9425
elonezra <ubuntu@ip-172-31-41-51.us-east-2.compute.internal>
2020-03-22 18:06:19 +0000

    v2

---
+++ Test.cpp
@@ -0,0 +1,16 @@
+#include "doctest.h"
+#include "PhoneticFinder.hpp"
+
+using namespace phonetic;
+
+TEST_CASE("Factorials of small numbers") {
+    CHECK(find("help","help") == "null");
+
+}
+
+TEST_CASE("Factorials of large numbers") {
+    
+    /* add more checks here */
+}
+
+/* add more test cases here */
