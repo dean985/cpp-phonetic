fd270eb7b6fc303a2824e7b838ea50ffa1f46ff8
Ubuntu <ubuntu@ip-172-31-41-51.us-east-2.compute.internal>
2020-03-22 17:57:45 +0000

    v1

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -1,9 +1,11 @@
+#include "PhoneticFinder.hpp"
 #include <string>
 using namespace std;
 
 namespace phonetic
 {
-    string find(string text, string word){
-        return " ";
+    string find(string text, string word)
+    {
+        return "null_test";
     }
 }
