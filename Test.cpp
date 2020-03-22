35459c023d963f9360ae63f834d94287f53b6f0f
elonezra <ubuntu@ip-172-31-41-51.us-east-2.compute.internal>
2020-03-22 18:39:27 +0000

    add Test_case title

--- Test.cpp
+++ Test.cpp
@@ -1,16 +1,87 @@
 #include "doctest.h"
 #include "PhoneticFinder.hpp"
 
+using namespace std;
 using namespace phonetic;
 
-TEST_CASE("Factorials of small numbers") {
-    CHECK(find("help","help") == "null");
-
-}
-
-TEST_CASE("Factorials of large numbers") {
+TEST_CASE("check if LOWER CASE transform work")
+{
+    string text = "Dond vorri be haffy";
     
-    /* add more checks here */
+    CHECK(find(text,"dond") == "Dond");
+    CHECK(find(text,"Dond") == "Dond");
+    CHECK(find(text,"DoNd") == "Dond");
+    
+    
+    CHECK(find(text,"Vorri") == "vori");
+    CHECK(find(text,"vorrI") == "vorri");
+    CHECK(find(text,"vorri") == "vorri");
+    
+    
+    CHECK(find(text,"BE") == "be");
+    CHECK(find(text,"bE") == "be");
+    
+   
+    CHECK(find(text,"haffy") == "haffy");
+    CHECK(find(text,"haFFy") == "haffy");
+    CHECK(find(text,"HAFFY") == "haffy");
+    CHECK(find(text,"Haffy") == "haffy");
+
 }
+
+
+TEST_CASE("check transform between v and w") 
+{
+    
+   
+}
+
+TEST_CASE("check transform between b, f, p") 
+{
+    
+   
+}
+TEST_CASE("check transform between g, j") 
+{
+    
+   
+}
+
+TEST_CASE("check transform between c, k, q") 
+{
+    
+   
+}
+
+TEST_CASE("check transform between s, z") 
+{
+    
+   
+}
+
+TEST_CASE("check transform between d, t") 
+{
+    
+   
+}
+
+TEST_CASE("check transform between o, u") 
+{
+    
+   
+}
+
+TEST_CASE("check transform between i, y") 
+{
+    
+   
+}
+
+TEST_CASE("check Exceptions") 
+{
+    
+   
+}
+
 
 /* add more test cases here */
