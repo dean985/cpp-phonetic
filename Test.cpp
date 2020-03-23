56f12b8b73a575b4145b4cca5488d94386e9cd6a
elonezra <ubuntu@ip-172-31-41-51.us-east-2.compute.internal>
2020-03-23 10:23:58 +0000

    elon's edit Test.cpp Test_case 234

--- Test.cpp
+++ Test.cpp
@@ -8,44 +8,92 @@ TEST_CASE("check if LOWER CASE transform work")
 {
     string text = "Dond vorri be haffy";
     
-    CHECK(find(text,"dond") == "Dond");
-    CHECK(find(text,"Dond") == "Dond");
-    CHECK(find(text,"DoNd") == "Dond");
+    CHECK(find(text,"dond") == string("Dond"));
+    CHECK(find(text,"Dond") == string("Dond"));
+    CHECK(find(text,"DoNd") == string("Dond"));
     
     
-    CHECK(find(text,"Vorri") == "vori");
-    CHECK(find(text,"vorrI") == "vorri");
-    CHECK(find(text,"vorri") == "vorri");
+    CHECK(find(text,"Vorri") == string("vori"));
+    CHECK(find(text,"vorrI") == string("vorri"));
+    CHECK(find(text,"vorri") == string("vorri"));
     
     
-    CHECK(find(text,"BE") == "be");
-    CHECK(find(text,"bE") == "be");
+    CHECK(find(text,"BE") == string("be"));
+    CHECK(find(text,"bE") == string( "be"));
     
    
-    CHECK(find(text,"haffy") == "haffy");
-    CHECK(find(text,"haFFy") == "haffy");
-    CHECK(find(text,"HAFFY") == "haffy");
-    CHECK(find(text,"Haffy") == "haffy");
+    CHECK(find(text,"haffy") == string("haffy"));
+    CHECK(find(text,"haFFy") == string("haffy"));
+    CHECK(find(text,"HAFFY") == string("haffy"));
+    CHECK(find(text,"Haffy") == string("haffy"));
 
 }
 
 
 TEST_CASE("check transform between v and w") 
 {
-    
+        string text = "Dond vorri be haffy";
+        
+        CHECK(find(text,"worri") == string("vorri"));
+        CHECK(find(text,"worry") == string("vorri"));
+        
+        
+        CHECK(find("vord and word","word") == string("word"));
+        CHECK(find("my name is volwerine","wolverine") == string("volwerine"));
+        CHECK(find("my name is wolwerine","wolverine") == string("wolwerine"));
+        
+        
+        CHECK(find("work for Vork","Vork") == string("Vork"));
+        CHECK(find("work for Vork","work") == string("work"));
+        
+        CHECK(find("twerk for Vork","work") == string("Vork"));
+
+        CHECK(find("ve vill we vill rock you","we") == "we");
+        CHECK(find("cov and cov were out","cow") == "cov");
    
 }
 
 TEST_CASE("check transform between b, f, p") 
 {
+
+        CHECK(find("Dond vorri be haffy","happy") == string("haffy"));
+        CHECK(find("fly into the pufpet","buffet") == string("pufpet"));
+        
+        CHECK(find("play and flay","play") == string("play"));
+        CHECK(find("play and flay","flay") == string("flay")); 
+        
+        CHECK(find("preferd perfect","preferd") == string("preferd"));
+        CHECK(find("breperd perfect","preferd") == string("breperd")); 
+        
+        CHECK(find("bap fap pap","pap") == string("pap"));
+        CHECK(find("broblem and not problem","broblem") == string("broblem")); 
+        
+        CHECK(find("club or clup","clup") == string("clup"));
+        CHECK(find("pice of faber","paper") == string("paber"));
     
    
 }
 TEST_CASE("check transform between g, j") 
 {
     
+        CHECK(find("give me jive me","give") == string("give"));
+        CHECK(find("jive me give me","give") == string("give"));
+        
+        CHECK(find("google moogle","joojle") == string("google"));
+        CHECK(find("goojle moogle","google") == string("goojle")); 
+        
+        CHECK(find("joogle moogle","google") == string("joogle"));
+        CHECK(find("jump and gump","gump") == string("gump")); 
+        
+        CHECK(find("jump and gump","jump") == string("jump"));
+        
+        CHECK(find("trump is jump","gump") == string("jump")); 
+        
+        CHECK(find("jennifer or gennifer","Jennifer") == string("jennifer"));
+        CHECK(find("gum gum jum","jum") == string("jum"));
    
 }
+//////////////////////////////////////////////
 
 TEST_CASE("check transform between c, k, q") 
 {
